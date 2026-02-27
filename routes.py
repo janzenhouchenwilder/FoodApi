b4f9b34f82a8a53ce98f917177b63d0a27c8e5bd
Janzen Houchen-Wilder <janzenhouchen-wilder@Janzens-MacBook-Pro.local>
2026-02-27 10:50:16 -0800

    Clean up and adding query parameters.

--- routes.py
+++ routes.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, jsonify
-import config
+from flask import Blueprint, jsonify, request
 import requests
 from dataclasses import asdict
 from TokenModel import Token
 import FoodModel
+from auth import get_access_token, invalidate_token
 
 api = Blueprint("api", __name__)
 
@@ -11,11 +11,17 @@ api = Blueprint("api", __name__)
 def health():
     return jsonify(status="ok")
     
-@api.route("/food/<foodName>")
-def food(foodName):
-    token = getCredentials()
-    requestData = FoodModel.FoodSearch(method="food.search", 
-                                       search_expression=foodName, 
+@api.route("/food/<food_name>")
+def food(food_name):
+    token = get_access_token()
+    breakpoint()
+    brand = request.args.get("brand")
+    search_exp = food_name
+    if brand:
+        search_exp = f'{food_name} "{brand}"'
+
+    request_data = FoodModel.FoodSearch(method="food.search", 
+                                       search_expression=search_exp, 
                                        format="json"
     )
     breakpoint()
@@ -25,23 +31,17 @@ def food(foodName):
         data=asdict(requestData), 
         timeout=20 
     )
+
+    if response.status_code == 401:
+        invalidate_token()
+        token = get_access_token()
+        response = requests.get(
+        "https://platform.fatsecret.com/rest/foods/search/v1", 
+        headers={"Authorization": f"{token.token_type} {token.access_token}"}, 
+        data=asdict(request_data), 
+        timeout=20 
+    )
+        
     response.raise_for_status()
     data = response.json()
     return jsonify(data)
-    
-def getCredentials() -> Token:
-    credentials = config.getCredentials()
-    response = requests.post(
-        "https://oauth.fatsecret.com/connect/token",
-        auth=(credentials["ClientId"], credentials["ClientSecret"]),
-        data={"grant_type": "client_credentials"},
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-        timeout=20
-    )
-    response.raise_for_status()
-    data = response.json()
-    return Token(access_token = data["access_token"],
-                token_type = data["token_type"],
-                expires_in = data["expires_in"]
-    )
-    
