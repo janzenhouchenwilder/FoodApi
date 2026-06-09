723180acb622d815ef1731fea572e31acb12b84c
Janzen Houchen-Wilder <janzenhouchen-wilder@Janzens-MacBook-Pro.local>
2026-06-08 17:54:53 -0700

    removing debugger stuff

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from routes import api
 import debugpy
 from config import Config
 
-debugpy.listen(("127.0.0.1", 5678))
-print("Debugger listening on 127.0.0.1:5678")
+#debugpy.listen(("127.0.0.1", 5678))
+#print("Debugger listening on 127.0.0.1:5678")
 
 app = Flask(__name__)
 app.config.from_object(Config)
